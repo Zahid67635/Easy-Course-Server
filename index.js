3e31ed8279392dbe8705d1ad601f042365cddf58
Zahid <zahid67635gmail.com>
2022-10-25 16:57:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const courses = require('./data/courses.json');
+
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Server running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(n => n.id === id);
+    res.send(selectedCourse);
+})
+
+app.listen(port, () => console.log('port is running'))
